ad64a0ee6aa99b56c469e185b9d86f1116732289
emilymeng <eeng@seattletimes.com>
2018-12-13 18:24:40 -0800

    removed unused parts

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-// require("./lib/ads");
-// var track = require("./lib/tracking");
+require("./lib/ads");
+var track = require("./lib/tracking");
+var paywall = require("./lib/paywall");
+setTimeout(() => paywall(11152084), 5000);
 
-// require("component-responsive-frame/child");
+
 require("./map");
 require("./tabs");
